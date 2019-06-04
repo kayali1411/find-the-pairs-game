3580e26acbd7bb8d6c3706a3b4d9adb02ca26862
ahmad.kayali <ahmad.kayali@sultan-center.com>
2019-06-05 00:53:06 +0300

    refactoring js code

--- public/js/app.js
+++ public/js/app.js
@@ -1,22 +1,62 @@
-(() => {
-    const emojiArray = ['cool', 'cry', 'flushed', 'in-love', 'kiss', 'mask', 'sleeping', 'smiling', 'tired', 'tongue', 'cool', 'cry', 'flushed', 'in-love', 'kiss', 'mask', 'sleeping', 'smiling', 'tired', 'tongue'];
+const emojiListBlock  = document.querySelector('.elements-list');
+const startGameButton = document.getElementById('start-game');
+const restGameButton  = document.getElementById('rest-game');
+const gameBlock       = document.getElementById('game-block');
+const matched         = document.getElementById('matched');
+const total           = document.getElementById('total');
+const moves           = document.getElementById('moves');
+const modal           = document.querySelector('.modal');
 
-    let matchedEmoji = [];
-    let openedEmoji = [];
+const Game = function() {
+    this.start = function() {
 
-    let matched = 0;
-    let moves = 0;
+        startGameButton.classList.add('disabled');
+        restGameButton.classList.remove('disabled');
+        emojiListBlock.classList.remove('disabled');
 
-    const initGame = () => {
-        const emojis = shuffleEmojis(emojiArray);
-        addEmoji(emojis);
-        initEvents();
-    };
-
-    const addEmoji = (emojis) => {
-        const emojiListBlock = document.querySelector('.elements-list');
+        const emojis = document.querySelectorAll('.emoji');
 
         emojis.forEach((emoji) => {
+            emoji.classList.toggle('flipped');
+            emoji.classList.toggle('disabled');
+        });
+
+        setTimeout(() => {
+            emojis.forEach((emoji) => {
+                emoji.classList.toggle('flipped');
+                emoji.classList.toggle('disabled');
+            });
+        }, 2000);
+    };
+
+    this.rest = function() {
+        modal.classList.remove('show');
+        this.player.init();
+        this.shuffleEmojis();
+        this.addEmojis();
+    };
+
+    this.incMatched = function() {
+        matched.textContent = ++this.player.hasMatched;
+    };
+
+    this.incMoves = function() {
+        moves.textContent = ++this.player.moves;
+
+    };
+
+    this.shuffleEmojis = function() {
+        for( let i = this.emojis.length-1; i >= 0; i-- ) {
+            let randomIndex = Math.floor(Math.random()*(i+1));
+            let itemAtIndex = this.emojis[randomIndex];
+            this.emojis[randomIndex] = this.emojis[i];
+            this.emojis[i] = itemAtIndex;
+        }
+    };
+
+    this.addEmojis = function() {
+        emojiListBlock.innerHTML = '';
+        this.emojis.forEach((emoji) => {
             let element = document.createElement('li');
             element.classList.add('element');
 
@@ -44,126 +84,96 @@
         });
     };
 
-    const startGame = () => {
-        restConfig();
-
-        let emojis = document.querySelectorAll('.emoji');
-
-        emojis.forEach((emoji) => {
-            emoji.classList.toggle('flipped');
-            emoji.classList.toggle('disabled');
-        });
-
-        setTimeout(() => {
-            emojis.forEach((emoji) => {
-                emoji.classList.toggle('flipped');
-                emoji.classList.toggle('disabled');
-            });
-        }, 2000);
-    };
-
-    const restGame = () => {
-        restConfig();
-
-        matchedEmoji = [];
-        openedEmoji = [];
-        document.querySelector('.elements-list').innerHTML = '';
-
-        document.getElementById('winner').classList.remove('show');
-        const emojis = shuffleEmojis(emojiArray);
-        addEmoji(emojis);
-    };
-
-    const initEvents = () => {
-        const startGameButton = document.getElementById('start-game');
-        startGameButton.addEventListener('click', () => startGame());
-
-        const restGameButton = document.getElementById('rest-game');
-        restGameButton.addEventListener('click', () => restGame());
-
-        const modal = document.getElementById('winner');
-        modal.addEventListener('click', () => restGame());
-
-        const game = document.getElementById('game-block');
-
-        game.addEventListener('click', (e) => {
-            if (e.target && e.target.nodeName === 'SPAN') {
-                let emoji = e.target.parentNode;
-                flipEmoji(emoji);
-            }
-        });
-    };
-
-    const flipEmoji = (emoji) => {
-        emoji.classList.toggle('flipped');
-        emoji.classList.toggle('disabled');
-
-        openedEmoji = openedEmoji.concat(emoji.getAttribute('data-emoji'));
-        if(openedEmoji.length === 2) {
-            document.querySelector('.elements-list').classList.add('disabled');
-            setTimeout(() => {
-                compareEmoji();
-            }, 1000);
-        }
-    };
-
-    const shuffleEmojis = (emojis) => {
-        let shuffled = [...emojis];
-        for( let i = shuffled.length-1; i >= 0; i-- ) {
-            let randomIndex = Math.floor(Math.random()*(i+1));
-            let itemAtIndex = shuffled[randomIndex];
-            shuffled[randomIndex] = shuffled[i];
-            shuffled[i] = itemAtIndex;
-        }
-        return shuffled;
-    };
-
-    const compareEmoji = () => {
-        if(openedEmoji[0] === openedEmoji[1]) {
-            matchedEmoji = matchedEmoji.concat(openedEmoji);
-            console.log(matchedEmoji.length, emojiArray.length);
-            incMatched();
+    this.compareEmoji = function() {
+        if(this.player.currentOpened[0] === this.player.currentOpened[1]) {
+            this.player.matchedEmoji = this.player.matchedEmoji.concat(this.player.currentOpened);
+            this.incMatched();
         } else {
             let emojis = document.querySelectorAll('.flipped');
             emojis.forEach((emoji) => {
-                if(!matchedEmoji.includes(emoji.getAttribute('data-emoji'))) {
+                if(!this.player.matchedEmoji.includes(emoji.getAttribute('data-emoji'))) {
                     emoji.classList.toggle('flipped');
                     emoji.classList.toggle('disabled');
                 }
             });
         }
-        openedEmoji = [];
-        incTries();
-        document.querySelector('.elements-list').classList.remove('disabled');
+        this.player.currentOpened = [];
+        this.incMoves();
+        emojiListBlock.classList.remove('disabled');
 
-        isWinner();
-    }
+        this.player.isWinner(this.emojis.length);
+    };
 
-    const restConfig = () => {
-        document.getElementById('start-game').classList.toggle('disabled');
-        document.getElementById('rest-game').classList.toggle('disabled');
-        document.querySelector('.elements-list').classList.toggle('disabled');
-        document.getElementById('matched').textContent = 0;
-        document.getElementById('moves').textContent = 0;
+    this.setupEvents = function() {
+        startGameButton.addEventListener('click', () => this.start());
 
-        matched = 0;
-        moves = 0;
-    }
+        restGameButton.addEventListener('click', () => this.rest());
 
-    const incMatched = () => {
-        document.getElementById('matched').textContent = ++matched;
+        modal.addEventListener('click', () => this.rest());
 
-    }
+        gameBlock.addEventListener('click', (e) => {
+            if (e.target && e.target.nodeName === 'SPAN') {
+                let emoji = e.target.parentNode;
+                this.player.flipEmoji(emoji);
+                if(this.player.currentOpened.length === 2) {
+                    emojiListBlock.classList.add('disabled');
+                    setTimeout(() => {
+                        this.compareEmoji();
+                    }, 1000);
+                }
+            }
+        });
+    };
 
-    const incTries = () => {
-        document.getElementById('moves').textContent = ++moves;
-    }
+    this.init = function(player) {
+        this.emojis = ['cool', 'cry', 'flushed', 'in-love', 'kiss', 'mask', 'sleeping', 'smiling', 'tired', 'tongue', 'cool', 'cry', 'flushed', 'in-love', 'kiss', 'mask', 'sleeping', 'smiling', 'tired', 'tongue'];
+        this.player = player;
+        this.player.init();
+        this.shuffleEmojis();
+        this.addEmojis();
+        this.setupEvents();
+    };
 
-    const isWinner = () => {
-        if(matchedEmoji.length === emojiArray.length) {
-            document.getElementById('winner').classList.add('show');
+    this.emojis = [];
+    this.player = null;
+
+};
+
+const Player = function() {
+    this.init = function() {
+        this.matchedEmoji  = [];
+        this.currentOpened = [];
+        this.hasMatched    = 0;
+        this.moves         = 0;
+
+        moves.textContent   = this.moves;
+        matched.textContent = this.hasMatched;
+
+        startGameButton.classList.remove('disabled');
+        restGameButton.classList.add('disabled');
+        emojiListBlock.classList.add('disabled');
+    };
+
+    this.isWinner = function (totalEmojis) {
+        if(this.matchedEmoji.length === totalEmojis) {
+            modal.classList.add('show');
         }
-    }
+    };
 
-    initGame();
-})()
+    this.flipEmoji = function(emoji) {
+        emoji.classList.toggle('flipped');
+        emoji.classList.toggle('disabled');
+
+        this.currentOpened = this.currentOpened.concat(emoji.getAttribute('data-emoji'));
+    };
+
+    this.matchedEmoji  = [];
+    this.currentOpened = [];
+    this.hasMatched    = 0;
+    this.moves         = 0;
+};
+
+const game   = new Game();
+const player = new Player();
+
+game.init(player);
